94db7f8fa1ac1abdbbd54070d11b8d82a2d279cc
Andrzej Baranski <andrzejbaranski@onet.eu>
2022-06-05 21:08:21 +0200

    Initial commit

---
+++ pch_andy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by allti on 05.06.2022.
+//
+
+#include "pch_andy.h"
---
+++ pch_andy.h
@@ -0,0 +1,8 @@
+//
+// Created by allti on 05.06.2022.
+//
+
+#ifndef ZADANIE4_ANDY_PCH_ANDY_H
+#define ZADANIE4_ANDY_PCH_ANDY_H
+
+#endif //ZADANIE4_ANDY_PCH_ANDY_H
