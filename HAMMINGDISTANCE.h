ca53160349cf3e3bf22cc10a4e6ffbdd56b09552
Andrzej Baranski <andrzejbaranski@onet.eu>
2022-06-13 21:20:47 +0200

    function creation hammingdistance

--- HAMMINGDISTANCE.h
+++ HAMMINGDISTANCE.h
@@ -5,4 +5,8 @@
 #ifndef ZADANIE4_ANDY_HAMMINGDISTANCE_H
 #define ZADANIE4_ANDY_HAMMINGDISTANCE_H
 
+#include <stdint.h>
+
+uint8_t hammingDistance(uint8_t n1, uint8_t n2);
+
 #endif //ZADANIE4_ANDY_HAMMINGDISTANCE_H
