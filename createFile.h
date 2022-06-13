2cb7c94afc202bed11dc515d5a0a1211e1ab6b7e
Andrzej Baranski <andrzejbaranski@onet.eu>
2022-06-13 23:05:16 +0200

    update changes

--- createFile.h
+++ createFile.h
@@ -2,12 +2,12 @@
 // Created by allti on 13.06.2022.
 //
 
-#ifndef BER_KALKULATOR_DATAFILES_H
-#define BER_KALKULATOR_DATAFILES_H
+#ifndef ZADANIE4_Andy_BEERCALCULATION_H
+#define ZADANIE4_Andy_BEERCALCULATION_H
 
 void createFilesForTest_1();
 void createFilesForTest_2();
 void createFilesForTest_3();
 
 
-#endif BER_KALKULATOR_DATAFILES_H
+#endif ZADANIE4_Andy_BEERCALCULATION_H
