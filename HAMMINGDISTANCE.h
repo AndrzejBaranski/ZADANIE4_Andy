de417475fc1bb3dd021054a9b702e77ea3830396
Andrzej Baranski <andrzejbaranski@onet.eu>
2022-06-13 23:05:44 +0200

    update changes

--- HAMMINGDISTANCE.h
+++ HAMMINGDISTANCE.h
@@ -2,8 +2,8 @@
 // Created by allti on 13.06.2022.
 //
 
-#ifndef BER_KALKULATOR_BER_COMPUTER_H
-#define BER_KALKULATOR_BER_COMPUTER_H
+#ifndef ZADANIE4_Andy_BEERCALCULATION_H
+#define ZADANIE4_Andy_BEERCALCULATION_H
 
 int calcWrongBits(char ch1, char ch2);
 
@@ -11,5 +11,5 @@ int calcWrongBits(char ch1, char ch2);
 
 
 
-#endif //BER_KALKULATOR_BER_COMPUTER_H
+#endif //ZADANIE4_Andy_BEERCALCULATION_H
 
