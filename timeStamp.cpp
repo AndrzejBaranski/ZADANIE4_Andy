6148ef519494414538323f40bf3762e752552e49
Andrzej Baranski <andrzejbaranski@onet.eu>
2022-06-15 18:52:50 +0200

    adding timestamp

---
+++ timeStamp.cpp
@@ -0,0 +1,12 @@
+//
+// Created by allti on 15.06.2022.
+//
+
+#include "timeStamp.h"
+
+double timePoint() {
+
+}
+double timeStamp(double start, double end) {
+    double diff = end - start;
+}
---
+++ timeStamp.h
@@ -0,0 +1,15 @@
+//
+// Created by allti on 15.06.2022.
+//
+
+#ifndef ZADANIE4_ANDY_TIMESTAMP_H
+#define ZADANIE4_ANDY_TIMESTAMP_H
+
+#include <numeric>
+#include <iomanip>
+#include <chrono>
+
+double timePoint();
+double timeStamp(double start, double end);
+
+#endif //ZADANIE4_ANDY_TIMESTAMP_H
